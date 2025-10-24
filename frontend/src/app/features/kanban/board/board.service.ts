4dead4ec4b5d4b53765689cdecd7dfdcd7339fcb
Rafael Gonçalves <rafaelgfra@hotmail.com>
2025-10-24 16:38:05 -0300

    fix: environments

--- frontend/src/app/features/kanban/board/board.service.ts
+++ frontend/src/app/features/kanban/board/board.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Columns } from '@type/types';
-import { environment } from '../../../../environments/environment.development';
+import { environment } from '../../../../environments/environment';
 
 
 @Injectable({ providedIn: 'root' })
